264764bd36fdd53ef28047f399983bc1b741b1aa
brandon <brandon@1942design.com>
2014-02-20 23:43:21 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+commandline
+===========
+*Node.js commandline interface.*  
+
+Run cli commands asynchronously, and if needed, get the output as a string or just run the command and ignore the output.
---
+++ commandline.js
@@ -0,0 +1,28 @@
+var sys     = require('sys'),
+    exec    = require('child_process').exec;
+
+var commandline={
+    get:getString,
+    run:runCommand
+};
+
+function runCommand(command){
+    exec(
+        command
+    );
+}
+
+function getString(command,callback){
+    exec(
+        command, 
+        (
+            function(){
+                return function(err,data,stderr){
+                    callback(data);
+                }
+            }
+        )(callback)
+    );
+}
+
+module.exports=commandline;
